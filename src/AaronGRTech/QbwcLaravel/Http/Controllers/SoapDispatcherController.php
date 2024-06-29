d4f16147a6053c4c2959ad56914cd908232bf5a0
Aaron Balentie <aaron@goldenruleweb.com>
2024-06-28 23:56:33 -0500

    refactor: Update SoapDispatcherController to use SoapService in handle method

--- src/AaronGRTech/QbwcLaravel/Http/Controllers/SoapDispatcherController.php
+++ src/AaronGRTech/QbwcLaravel/Http/Controllers/SoapDispatcherController.php
@@ -3,6 +3,7 @@
 namespace AaronGRTech\QbwcLaravel\Http\Controllers;
 
 use AaronGRTech\QbwcLaravel\Services\SoapService;
+use AaronGRTech\QbwcLaravel\StructType\ServerVersion;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use SoapServer;
@@ -10,24 +11,33 @@ use SoapServer;
 class SoapDispatcherController extends Controller
 {
     protected $options;
+    protected $soapService;
 
-    public function __construct()
+    public function __construct(SoapService $soapService)
     {
         $this->options = [
             'uri' => url(config('qbwc.routes.prefix')),
             'classmap' => \AaronGRTech\QbwcLaravel\ClassMap::get(),
         ];
+        $this->soapService = $soapService;
     }
+
 
     public function handle()
     {
         $server = new SoapServer(config('qbwc.soap.wsdl'), $this->options);
-        $server->setObject(new SoapService());
+        $server->setObject($this);
 
         ob_start();
         $server->handle();
         $response = ob_get_clean();
 
         return response($response, 200)->header('Content-Type', 'text/xml; charset=UTF-8');
+    }
+
+    public function serverVersion($parameters)
+    {
+        $version = new ServerVersion();
+        return $this->soapService->serverVersion($version);
     }
 }
