218982080d7eb0db5ac58713cd3bd3b16ef7e00b
Aaron Balentie <aaron@goldenruleweb.com>
2024-07-04 11:22:54 -0500

    Pathing fix

--- src/Providers/SoapServiceProvider.php
+++ src/Providers/SoapServiceProvider.php
@@ -27,37 +27,37 @@ class SoapServiceProvider extends ServiceProvider
         $packageBaseDir = dirname(__DIR__, 2);
 
         $this->publishes([
-            $packageBaseDir . '/wsdl/QBWebConnectorSvc.wsdl' => storage_path('app/wsdl/QBWebConnectorSvc.wsdl'),
+            $packageBaseDir . '/src/wsdl/QBWebConnectorSvc.wsdl' => storage_path('app/wsdl/QBWebConnectorSvc.wsdl'),
         ], 'qbwc-wsdl');
 
         $this->publishes([
-            $packageBaseDir . '/config/qbwc.php' => config_path('qbwc.php'),
+            $packageBaseDir . '/src/config/qbwc.php' => config_path('qbwc.php'),
         ], 'qbwc-config');
 
-        $this->loadMigrationsFrom(__DIR__ . '/../Migrations');
+        $this->loadMigrationsFrom($packageBaseDir . '/src/Migrations');
 
         $this->publishes([
-            __DIR__ . '/../Migrations/create_queues_table.php' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_queues_table.php'),
-            __DIR__ . '/../Migrations/create_tasks_table.php' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_tasks_table.php'),
-            __DIR__ . '/../Migrations/create_task_configs_table.php' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_task_configs_table.php'),
+            $packageBaseDir . '/src/Migrations/create_queues_table.php' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_queues_table.php'),
+            $packageBaseDir . '/src/Migrations/create_tasks_table.php' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_tasks_table.php'),
+            $packageBaseDir . '/src/Migrations/create_task_configs_table.php' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_task_configs_table.php'),
         ], 'qbwc-migrations');
 
         $this->registerRoutes();
 
         $this->publishes([
-            $packageBaseDir . '/Callbacks/QbwcCallback.php' => app_path('Callbacks/QbwcCallback.php'),
-            $packageBaseDir . '/Callbacks/InvoiceCallback.php' => app_path('Callbacks/InvoiceCallback.php'),
+            $packageBaseDir . '/src/Callbacks/QbwcCallback.php' => app_path('Callbacks/QbwcCallback.php'),
+            $packageBaseDir . '/src/Callbacks/InvoiceCallback.php' => app_path('Callbacks/InvoiceCallback.php'),
         ], 'qbwc-callbacks');
 
         $this->publishes([
-            $packageBaseDir . '/Models/Qbwc/Queue.php' => app_path('Models/Qbwc/Queue.php'),
-            $packageBaseDir . '/Models/Qbwc/Task.php' => app_path('Models/Qbwc/Task.php'),
-            $packageBaseDir . '/Models/Qbwc/TaskConfig.php' => app_path('Models/Qbwc/TaskConfig.php'),
+            $packageBaseDir . '/src/Models/Qbwc/Queue.php' => app_path('Models/Qbwc/Queue.php'),
+            $packageBaseDir . '/src/Models/Qbwc/Task.php' => app_path('Models/Qbwc/Task.php'),
+            $packageBaseDir . '/src/Models/Qbwc/TaskConfig.php' => app_path('Models/Qbwc/TaskConfig.php'),
         ], 'qbwc-models');
 
         // Publish other resources like controllers if needed
         $this->publishes([
-            $packageBaseDir . '/Http/Controllers/SoapController.php' => app_path('Http/Controllers/Qbwc/SoapController.php'),
+            $packageBaseDir . '/src/Http/Controllers/SoapController.php' => app_path('Http/Controllers/Qbwc/SoapController.php'),
         ], 'qbwc-controllers');
     }
 
